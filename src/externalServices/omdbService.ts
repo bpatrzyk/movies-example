194a1ff871987e25fd5932c03fdc3b5beffdb311
Bartłomiej Patrzyk <bpatrzyk@users.noreply.github.com>
2019-11-20 09:56:55 +0100

    OMDB API service stub

---
+++ src/externalServices/omdbService.ts
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+import url from 'url';
+
+const API_URL = 'http://www.omdbapi.com';
+const API_KEY = process.env.OMDB_API_KEY;
+
+export interface OMDBMovie {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: Rating[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
+}
+
+export interface Rating {
+  Source: string;
+  Value: string;
+}
+
+export async function getMovie(title: string) {
+  const queryUrl = url.parse(`${API_URL}/?apikey=${API_KEY}&t=${title}`);
+  const response = await fetch(queryUrl);
+  return await response.json() as OMDBMovie;
+}
