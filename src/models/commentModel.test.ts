d5d922a8f559e364f7bb817a41688d3d4556fd70
Bartłomiej Patrzyk <bpatrzyk@users.noreply.github.com>
2019-11-21 17:44:38 +0100

    Refactor commentModel tests

--- src/models/commentModel.test.ts
+++ src/models/commentModel.test.ts
@@ -1,6 +1,18 @@
 import { db } from './db';
+import { getComments, createComment, Comment, CommentData } from './commentModel';
 
-import { getComments, createComment } from './commentModel';
+const comment = {
+  id: 'some id',
+  movie_id: 'movie id',
+  username: 'user123',
+  comment: 'some comment',
+} as Comment;
+
+const commentData = {
+  movie_id: 'movie id',
+  username: 'user123',
+  comment: 'some comment',
+} as CommentData;
 
 describe('commentModel', () => {
   beforeEach(() => {
@@ -10,50 +22,29 @@ describe('commentModel', () => {
   describe('getComments', () => {
     it('selects all comments from db', async () => {
       (db().then as jest.Mock).mockImplementationOnce(done => {
-        done([{
-          id: 'some id',
-          movie_id: 'movie id',
-          username: 'user123',
-          comment: 'some comment',
-        }]);
+        done([comment]);
       });
+
       const comments = await getComments();
+
       expect(db).toBeCalledWith('comments');
       expect(db('comments').select).toBeCalledWith();
-      expect(comments).toEqual([{
-        id: 'some id',
-        movie_id: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      }]);
+      expect(comments).toEqual([comment]);
     });
   });
 
   describe('createComment', () => {
     it('creates comment in db', async () => {
       (db().then as jest.Mock).mockImplementationOnce(done => {
-        done([{
-          id: 'some id',
-          movie_id: 'movie id',
-          username: 'user123',
-          comment: 'some comment',
-        }]);
+        done([comment]);
       });
-      const comment = {
-        movie_id: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      };
-      const createdMovie = await createComment(comment);
+
+      const createdMovie = await createComment(commentData);
+
       expect(db).toBeCalledWith('comments');
-      expect(db('comments').insert).toBeCalledWith(comment);
+      expect(db('comments').insert).toBeCalledWith(commentData);
       expect(db('comments').returning).toBeCalledWith('*');
-      expect(createdMovie).toEqual({
-        id: 'some id',
-        movie_id: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      })
+      expect(createdMovie).toEqual(comment);
     });
   });
 });
