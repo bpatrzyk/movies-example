710b624ab047fcc793d8920fd6de11caddebf4fc
Bartłomiej Patrzyk <bpatrzyk@users.noreply.github.com>
2019-11-21 12:30:01 +0100

    Add commentsValidator tests

---
+++ src/validators/commentsValidator.test.ts
@@ -0,0 +1,135 @@
+import httpMocks from 'node-mocks-http';
+import { validationResult } from 'express-validator';
+import { testExpressValidatorMiddleware } from '../utils/validator';
+import { postCommentValidator } from './commentsValidator';
+
+describe('commentsValidator', () => {
+  describe('movieId validation', () => {
+    it('checks if movieId is not empty', async () => {
+      const body = {
+        comment: 'some comment',
+        username: 'user123',
+      };
+      const { req, res } = httpMocks.createMocks({ body }, {});
+      await testExpressValidatorMiddleware(req, res, postCommentValidator);
+      const result = validationResult(req);
+      expect(result.array()).toEqual([
+        {
+          value: undefined,
+          msg: 'Invalid value',
+          param: 'movieId',
+          location: 'body',
+        },
+        {
+          value: '',
+          msg: 'Invalid value',
+          param: 'movieId',
+          location: 'body',
+        },
+      ]);
+    });
+
+    it('trims the movieId', async () => {
+      const body = {
+        movieId: '     some id   ',
+        comment: 'some comment',
+        username: 'user123',
+      };
+      const { req, res } = httpMocks.createMocks({ body }, {});
+      await testExpressValidatorMiddleware(req, res, postCommentValidator);
+      const result = validationResult(req);
+      expect(result.array()).toEqual([]);
+      expect(req.body).toEqual({
+        movieId: 'some id',
+        comment: 'some comment',
+        username: 'user123',
+      });
+    });
+  });
+
+  describe('comment validation', () => {
+    it('checks if comment is not empty', async () => {
+      const body = {
+        movieId: 'some id',
+        username: 'user123',
+      };
+      const { req, res } = httpMocks.createMocks({ body }, {});
+      await testExpressValidatorMiddleware(req, res, postCommentValidator);
+      const result = validationResult(req);
+      expect(result.array()).toEqual([
+        {
+          value: undefined,
+          msg: 'Invalid value',
+          param: 'comment',
+          location: 'body',
+        },
+        {
+          value: '',
+          msg: 'Invalid value',
+          param: 'comment',
+          location: 'body',
+        },
+      ]);
+    });
+
+    it('trims the username', async () => {
+      const body = {
+        movieId: 'some id',
+        comment: '    some comment   ',
+        username: 'user123',
+      };
+      const { req, res } = httpMocks.createMocks({ body }, {});
+      await testExpressValidatorMiddleware(req, res, postCommentValidator);
+      const result = validationResult(req);
+      expect(result.array()).toEqual([]);
+      expect(req.body).toEqual({
+        movieId: 'some id',
+        comment: 'some comment',
+        username: 'user123',
+      });
+    });
+  });
+
+  describe('username validation', () => {
+    it('checks if username is not empty', async () => {
+      const body = {
+        movieId: 'some id',
+        comment: 'some comment',
+      };
+      const { req, res } = httpMocks.createMocks({ body }, {});
+      await testExpressValidatorMiddleware(req, res, postCommentValidator);
+      const result = validationResult(req);
+      expect(result.array()).toEqual([
+        {
+          value: undefined,
+          msg: 'Invalid value',
+          param: 'username',
+          location: 'body',
+        },
+        {
+          value: '',
+          msg: 'Invalid value',
+          param: 'username',
+          location: 'body',
+        },
+      ]);
+    });
+
+    it('trims the username', async () => {
+      const body = {
+        movieId: 'some id',
+        comment: 'some comment',
+        username: '    user123   ',
+      };
+      const { req, res } = httpMocks.createMocks({ body }, {});
+      await testExpressValidatorMiddleware(req, res, postCommentValidator);
+      const result = validationResult(req);
+      expect(result.array()).toEqual([]);
+      expect(req.body).toEqual({
+        movieId: 'some id',
+        comment: 'some comment',
+        username: 'user123',
+      });
+    });
+  });
+});
