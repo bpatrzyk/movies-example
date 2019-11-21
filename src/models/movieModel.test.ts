d8da695ff2f703cac354634eca64a3144b252062
Bartłomiej Patrzyk <bpatrzyk@users.noreply.github.com>
2019-11-21 17:48:06 +0100

    Refactor movieModel tests

--- src/models/movieModel.test.ts
+++ src/models/movieModel.test.ts
@@ -1,6 +1,21 @@
 import { db } from './db';
 
-import { getMovies, createMovie, exists } from './movieModel';
+import { getMovies, createMovie, exists, Movie, MovieData } from './movieModel';
+
+const movie = {
+  id: 'some id',
+  title: 'some title',
+  year: 1994,
+  genre: 'Comedy',
+  country: 'US',
+} as Movie;
+
+const movieData = {
+  title: 'some title',
+  year: 1994,
+  genre: 'Comedy',
+  country: 'US',
+} as MovieData;
 
 describe('movieModel', () => {
   beforeEach(() => {
@@ -10,55 +25,29 @@ describe('movieModel', () => {
   describe('getMovies', () => {
     it('selects all movies from db', async () => {
       (db().then as jest.Mock).mockImplementationOnce(done => {
-        done([{
-          id: 'some id',
-          title: 'some title',
-          year: 1994,
-          genre: 'Comedy',
-          country: 'US',
-        }]);
+        done([movie]);
       });
+
       const movies = await getMovies();
+
       expect(db).toBeCalledWith('movies');
       expect(db('movies').select).toBeCalledWith();
-      expect(movies).toEqual([{
-        id: 'some id',
-        title: 'some title',
-        year: 1994,
-        genre: 'Comedy',
-        country: 'US',
-      }]);
+      expect(movies).toEqual([movie]);
     });
   });
 
   describe('createMovie', () => {
     it('creates movie in db', async () => {
       (db().then as jest.Mock).mockImplementationOnce(done => {
-        done([{
-          id: 'some id',
-          title: 'some title',
-          year: 1994,
-          genre: 'Comedy',
-          country: 'US',
-        }]);
+        done([movie]);
       });
-      const movie = {
-        title: 'some title',
-        year: 1994,
-        genre: 'Comedy',
-        country: 'US',
-      };
-      const createdMovie = await createMovie(movie);
+
+      const createdMovie = await createMovie(movieData);
+
       expect(db).toBeCalledWith('movies');
-      expect(db('movies').insert).toBeCalledWith(movie);
+      expect(db('movies').insert).toBeCalledWith(movieData);
       expect(db('movies').returning).toBeCalledWith('*');
-      expect(createdMovie).toEqual({
-        id: 'some id',
-        title: 'some title',
-        year: 1994,
-        genre: 'Comedy',
-        country: 'US',
-      })
+      expect(createdMovie).toEqual(movie);
     });
   });
 
@@ -67,7 +56,9 @@ describe('movieModel', () => {
       (db().then as jest.Mock).mockImplementationOnce(done => {
         done([{}]);
       });
+
       const result = await exists('some movie');
+
       expect(db).toBeCalledWith('movies');
       expect(db('movies').select).toBeCalledWith(1);
       expect(db('movies').where).toBeCalledWith({ title: 'some movie' });
@@ -78,7 +69,9 @@ describe('movieModel', () => {
       (db().then as jest.Mock).mockImplementationOnce(done => {
         done([]);
       });
+
       const result = await exists('some movie');
+
       expect(db).toBeCalledWith('movies');
       expect(db('movies').select).toBeCalledWith(1);
       expect(db('movies').where).toBeCalledWith({ title: 'some movie' });
