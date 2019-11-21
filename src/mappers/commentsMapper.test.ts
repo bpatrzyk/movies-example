eacfbead922ec84ef0001ab41c5fecb9e917d028
Bartłomiej Patrzyk <bpatrzyk@users.noreply.github.com>
2019-11-21 16:19:44 +0100

    Refactor commentsMapper tests

--- src/mappers/commentsMapper.test.ts
+++ src/mappers/commentsMapper.test.ts
@@ -1,55 +1,52 @@
 import * as mapper from './commentsMapper';
+import { CommentDTO, NewCommentDTO } from '../dtos/commentDTOs';
+import { Comment, CommentData } from '../models/commentModel';
+
+const newCommentDTO = {
+  movieId: 'movie id',
+  username: 'user123',
+  comment: 'some comment',
+} as NewCommentDTO;
+
+const commentData = {
+  movie_id: 'movie id',
+  username: 'user123',
+  comment: 'some comment',
+} as CommentData;
+
+const comment = {
+  id: 'comment id',
+  movie_id: 'movie id',
+  username: 'user123',
+  comment: 'some comment',
+} as Comment;
+
+const commentDTO = {
+  id: 'comment id',
+  movieId: 'movie id',
+  username: 'user123',
+  comment: 'some comment',
+} as CommentDTO;
 
 describe('commentsMapper', () => {
   describe('toCommentData', () => {
     it('maps newCommentDTO to CommentData', () => {
-      const newCommentDTO = {
-        movieId: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      };
-      const commentData = mapper.toCommentData(newCommentDTO);
-      expect(commentData).toEqual({
-        movie_id: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      });
+      const result = mapper.toCommentData(newCommentDTO);
+      expect(result).toEqual(commentData);
     });
   });
 
   describe('toCommentDTO', () => {
     it('should map Movie to MovieDTO', () => {
-      const comment = {
-        id: 'comment id',
-        movie_id: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      };
-      const commentDTO = mapper.toCommentDTO(comment);
-      expect(commentDTO).toEqual({
-        id: 'comment id',
-        movieId: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      });
+      const result = mapper.toCommentDTO(comment);
+      expect(result).toEqual(commentDTO);
     });
   });
 
   describe('toCommentsDTO', () => {
     it('should map Movie array to MovieDTO array', () => {
-      const comment = {
-        id: 'comment id',
-        movie_id: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      };
-      const commentDTO = mapper.toCommentsDTO([comment]);
-      expect(commentDTO).toEqual([{
-        id: 'comment id',
-        movieId: 'movie id',
-        username: 'user123',
-        comment: 'some comment',
-      }]);
+      const result = mapper.toCommentsDTO([comment]);
+      expect(result).toEqual([commentDTO]);
     });
   });
 });
